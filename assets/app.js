d5821c5a17aa4e312c9bee2947dcfb0809d0aebc
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-05 20:11:10 +0100

    edit app.js

--- assets/app.js
+++ assets/app.js
@@ -49,6 +49,6 @@ app.get('/', function(req, res) {
 //});
 
 
-app.listen(3306, function(){
-        console.log('Running on 3306');
+app.listen(3000, function(){
+        console.log('Running on 3000');
 });
