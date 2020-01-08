a466f2a3c58a3d5b7e929676109dba1e554c4e2f
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-08 17:47:22 +0100

    asd

--- assets/app.js
+++ assets/app.js
@@ -146,7 +146,7 @@ function queryArrayfromDB(){
   var getData = '';
 
   for (var i = 0; i <= querySent.lenght; i++) {
-    getData += querySent[i].toString();
+    getData += querySent[i].anzahl + ',';
   }
   console.log(getData);
 
