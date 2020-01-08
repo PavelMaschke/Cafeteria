45c479bf15469e5291762bbceffa27670b49db8e
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-08 17:44:37 +0100

    asd

--- assets/app.js
+++ assets/app.js
@@ -139,7 +139,7 @@ function queryArrayfromDB(){
     querySent = results;
     //console.log(querySent);
     console.log('querySent: ' + typeof(querySent))
-    console.log('querySent: ' + typeof(querySent[1]))
+    console.log('querySent2: ' + typeof(querySent[1].anzahl))
 
   });
 
