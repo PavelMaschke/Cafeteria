e160e7d74f1682e322854d2201cb9e2b24f4bdaf
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-10 17:00:32 +0100

    richtig

--- assets/app.js
+++ assets/app.js
@@ -12,16 +12,13 @@ app.use('/assets', express.static('assets/'));
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const db = mysql.createConnection({
-  host    : 'localhost', 
+  host    : 'localhost',
   user    : 'cafeteria',
   password: 'HWHBEYFPMAFkrxuip6qufuAz',
   database: 'cafeteria'
 });
 
-<<<<<<< HEAD
-=======
 //damit auf function gewartet werdwen kann
->>>>>>> cc9ad38cbf55902b531a828b16005feab3754c36
 db.asyncquery = util.promisify(db.query).bind(db);
 
 //connect
@@ -30,26 +27,22 @@ db.connect(function(err){
   console.log('connected');
 });
 
+
 app.get('/', function(req, res){
   res.render('index');
 });
 
 app.get('/bestandsliste', async function(req, res){
-	console.log(test);
-  	res.render('bestandsliste');
+  res.render('bestandsliste');
 });
 
 app.post('/bestandsliste', urlencodedParser, function(req, res) {
-
 
   let msg = stringToArray(req.body.x);
   console.log(msg);
   queryArrayToDB(msg);
 });
 
-<<<<<<< HEAD
-app.get('/einkaufsliste', async function(req, res){
-=======
 app.post('/addedRows', urlencodedParser, function(req, res) {
 
   for (var i = 0; i < req.body.length(); i++) {
@@ -60,25 +53,8 @@ app.post('/addedRows', urlencodedParser, function(req, res) {
         if (err) throw err;
     });
   }
-
-  console.log(msg);
-  queryArrayToDB(msg);
 });
 
-app.get('/einkaufsliste', function(req, res){
->>>>>>> cc9ad38cbf55902b531a828b16005feab3754c36
-
-<<<<<<< HEAD
-  bestand = await db.asyncquery('SELECT anzahl FROM bestand;');
-  console.log(bestand);
-  res.render('einkaufsliste', {dbValues: bestand});
-  
-=======
-  let valuesFromDB = queryStringfromDB();
-  console.log(valuesFromDB);
-  res.render('einkaufsliste', {dbValues: valuesFromDB});
->>>>>>> fe562d7d86c9852b3ba6879bdf90ff4630a3f46d
-});
 
 function stringToArray(str){
   var  ergebnis= str.split(',');
