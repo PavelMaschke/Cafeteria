6db76d51a42129faf4e6388a2570ad082d7e5536
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-11 16:42:43 +0100

    asd

--- assets/generatePdf.js
+++ assets/generatePdf.js
@@ -1,4 +1,29 @@
-var doc = new jsPDF()
+//var doc = new jsPDF()
 
-doc.text('Hello world!', 10, 10);
-doc.save('a4.pdf');
+//doc.text('Hello world!', 10, 10);
+//doc.save('a4.pdf');
+
+function createPDF() {
+  var sTable = document.getElementById('ta').innerHTML;
+
+  var style = "<style>";
+  style = style + "table {width: 100%;font: 17px Calibri;}";
+  style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+  style = style + "padding: 2px 3px;text-align: center;}";
+  style = style + "</style>";
+
+  // CREATE A WINDOW OBJECT.
+  var win = window.open('', '', 'height=700,width=700');
+
+  win.document.write('<html><head>');
+  win.document.write('<title>Profile</title>');   // <title> FOR PDF HEADER.
+  win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
+  win.document.write('</head>');
+  win.document.write('<body>');
+  win.document.write(sTable);         // THE TABLE CONTENTS INSIDE THE BODY TAG.
+  win.document.write('</body></html>');
+
+  win.document.close(); 	// CLOSE THE CURRENT WINDOW.
+
+  win.print();    // PRINT THE CONTENTS.
+}
