eb5adda683fe1171aad3ee121399e27062a6c209
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-12 14:39:19 +0100

    asd

--- assets/app.js
+++ assets/app.js
@@ -110,7 +110,7 @@ app.post('/updateNrmlBestand', urlencodedParser, function(req, res){
   let msg = stringToArray(req.body.x);
 
   msg.forEach(function(item, index){
-    let sql = 'UPDATE bestand SET normal = '+ item +' WHERE id ='+ index + 1 +';';
+    let sql = 'UPDATE bestand SET normal = '+ item +' WHERE id ='+ (index + 1) +';';
     console.log(sql);
 
     db.query(sql, function(err, results) {
