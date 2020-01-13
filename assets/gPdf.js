9659e1e69e2e469437799cb4cb3f7a401583e50b
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-13 17:47:00 +0100

    astaec

--- assets/gPdf.js
+++ assets/gPdf.js
@@ -1,7 +1,7 @@
 function createPDF2(){
   var lenght = document.getElementById("ta").rows.length;
 
-    document.getElementByTag(button).style.display = "none";
+    document.getElementByTag('button').style.display = "none";
 
   window.setTimeout('window.print();',1000);
 
