ea168500de53a32b5e9f23c6f04eac4af3ff996d
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-13 17:51:40 +0100

    ter

--- assets/gPdf.js
+++ assets/gPdf.js
@@ -1,10 +1,10 @@
 function createPDF2(){
-  var lenght = document.getElementById("ta").rows.length;
 
   var x = document.getElementsByTagName('button');
 
   for (var i = 0; i < x.length; i++) {
     x[i].style.display = "none";
+    x[i].style.visibility = "hidden";
   }
 
   window.setTimeout('window.print();',1000);
