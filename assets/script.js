5b537991d0c7fa8e38acc25663a5a50fbd697297
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-13 20:55:47 +0100

    asd

--- assets/script.js
+++ assets/script.js
@@ -148,7 +148,7 @@ function hinzufuegen5(text, bestellen, anzahl, normal, menge) {
     $("table").append(
       '<tr>' +
         '<td>'+ text +'</td>' +
-        '<td><button type=button onclick=buttonRemove('+ a +')>-</button><input type="text" name="none" id="'+ aid +'" value="'+ bestellen +'"><p id="'+ aid2 +'"></p><button type=button onclick=buttonAdd('+ a +')>+</button></td>' +
+        '<td><button type=button onclick=buttonRemove('+ a +')>-</button><input type="text" name="none" id="'+ aid +'" value="'+ bestellen +'"><p class="weg" id="'+ aid2 +'"></p><button type=button onclick=buttonAdd('+ a +')>+</button></td>' +
         '<td><p>'+ anzahl +'</p></td>' +
         '<td><p>'+ normal +'</p></td>' +
         '<td class=stck>'+ menge +'</td>' +
