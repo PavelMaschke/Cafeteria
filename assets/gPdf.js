87ccd515ccedfe70fc3fb5a9a96083f566a2be35
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-13 17:42:35 +0100

    test pdf

--- assets/gPdf.js
+++ assets/gPdf.js
@@ -1,4 +1,9 @@
 function createPDF2(){
+  var lenght = document.getElementById("ta").rows.length;
 
-  window.print();    // PRINT THE CONTENTS.
+  for (var i = 0; i < length; i++) {
+    document.getElementById(lenght + "1").style.display = "none";
+  }
+
+  window.print();
 }
