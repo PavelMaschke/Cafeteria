09d840c024ab44974459195a01fa879ec9de9bcc
ToddxD <51798868+ToddxD@users.noreply.github.com>
2020-01-13 20:22:47 +0100

    asd

--- assets/generatePdf.js
+++ assets/generatePdf.js
@@ -1,6 +1,7 @@
 function createPDF2() {
   //Tabelle angeben
   var sTable = document.getElementById('drucken').innerHTML;
+  console.log(sTable);
 
   //style angeben (+ und - buttons sollen nicht angezeigt werden)
   var style = "<style>";
